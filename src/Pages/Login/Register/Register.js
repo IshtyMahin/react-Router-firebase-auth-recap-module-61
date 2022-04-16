0b4e20d57cb213a1fc373d4ed18ecd1a1377a0c1
Ishtiaq uddin <ishty119@gmail.com>
2022-04-16 21:48:05 +0600

    set env variable

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -11,6 +11,9 @@ const Register = () => {
 
     const handleRegister = event => {
         event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
     }
 
   return (
