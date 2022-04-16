f8c7a957a2327e268bfc744936ac5488bf43851c
Ishtiaq uddin <ishty119@gmail.com>
2022-04-16 21:26:36 +0600

    module-61

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2 className='text-center text-primary'>Mechanic is sleepinng</h2>
+        </div>
+    );
+};
+
+export default NotFound;
